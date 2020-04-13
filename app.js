8335d78d87020ed441031cd282a681d50e2d1bd8
elyfialkoff <mouseen92@gmail.com>
2020-04-12 21:36:48 -0600

    first iteration of game complete

--- app.js
+++ app.js
@@ -17,17 +17,95 @@ const game = () => {
     const options = document.querySelectorAll('.options button');
     const playerHand = document.querySelector('.player-hand');
     const computerHand = document.querySelector('.computer-hand');
+    const hands = document.querySelectorAll('.hands img');
+
+    hands.forEach((hand) => {
+      hand.addEventListener('animationend', function() {
+        this.style.animation = '';
+      });
+    });
 
     // Computer Options
     const computerOptions = ['rock', 'paper', 'scissors'];
 
     options.forEach((option) => {
       option.addEventListener('click', function() {
-        console.log(this);
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber]
+
+        setTimeout(() => {
+          compareHands(this.textContent, computerChoice);
+
+          playerHand.src = `./images/${this.textContent}.png`;
+          computerHand.src = `./images/${computerChoice}.png`;
+        }, 2000);
+
+        playerHand.style.animation = 'shakePlayer 2s ease';
+        computerHand.style.animation = 'shakeComputer 2s ease';
       })
     })
 
-    const computerNumber = Math.floor(Math.random() * 3);
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector('.player-score p');
+    const computerScore = document.querySelector('.computer-score p');
+
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+
+  }
+
+  const compareHands = (playerChoice, computerChoice) => {
+    
+    const winner = document.querySelector('.winner');
+
+    if (playerChoice === computerChoice) {
+      winner.textContent = 'It is a tie!';
+      return;
+    }
+
+    if (playerChoice === 'rock') {
+      if (computerChoice === 'scissors') {
+        winner.textContent = 'Player Wins!';
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = 'Computer Wins!';
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+
+    if (playerChoice === 'scissors') {
+      if (computerChoice === 'paper') {
+        winner.textContent = 'Player Wins!';
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = 'Computer Wins!';
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+
+    if (playerChoice === 'paper') {
+      if (computerChoice === 'rock') {
+        winner.textContent = 'Player Wins!';
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = 'Computer Wins!';
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
 
   };
 
